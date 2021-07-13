6be3bc0184251f9b004fb8addd301e0d5329461d
sergioromeroo <manusergio96@gmail.com>
2021-07-13 20:51:30 -0300

    me falto el app.use listooo

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ const port = 3000;
 app.use(express.static("public"));
 
 const mainRouter = require("./routes/main");
+app.use("/",mainRouter)
 
 app.listen(port, () => console.log(" en el puerto " + port)); 
